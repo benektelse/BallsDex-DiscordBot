a2df7b5b5ada97fe7dace741630d3a415dde8823
benektelse <151446166+benektelse@users.noreply.github.com>
2024-05-08 04:14:32 -0700

    changed the font and updated the card generation
    changed how large some text was to let longer abilities look normal
    changed the spacing between the text to make the font look better
    font was changed to Pusab to better fit the theme of the dex (Geometry Dash uses the Pusab font, IconDex is based on Geometry Dash)

--- ballsdex/core/image_generator/image_gen.py
+++ ballsdex/core/image_generator/image_gen.py
@@ -19,11 +19,11 @@ RECTANGLE_HEIGHT = (HEIGHT // 5) * 2
 CORNERS = ((34, 261), (1393, 992))
 artwork_size = [b - a for a, b in zip(*CORNERS)]
 
-title_font = ImageFont.truetype(str(SOURCES_PATH / "ArsenicaTrial-Extrabold.ttf"), 170)
-capacity_name_font = ImageFont.truetype(str(SOURCES_PATH / "Bobby Jones Soft.otf"), 110)
-capacity_description_font = ImageFont.truetype(str(SOURCES_PATH / "OpenSans-Semibold.ttf"), 75)
+title_font = ImageFont.truetype(str(SOURCES_PATH / "ArsenicaTrial-Extrabold.ttf"), 165)
+capacity_name_font = ImageFont.truetype(str(SOURCES_PATH / "Bobby Jones Soft.otf"), 90)
+capacity_description_font = ImageFont.truetype(str(SOURCES_PATH / "OpenSans-Semibold.ttf"), 55)
 stats_font = ImageFont.truetype(str(SOURCES_PATH / "Bobby Jones Soft.otf"), 130)
-credits_font = ImageFont.truetype(str(SOURCES_PATH / "arial.ttf"), 40)
+credits_font = ImageFont.truetype(str(SOURCES_PATH / "arial.ttf"), 30)
 
 
 def draw_card(ball_instance: "BallInstance"):
@@ -40,26 +40,26 @@ def draw_card(ball_instance: "BallInstance"):
     icon = Image.open("." + ball.cached_economy.icon) if ball.cached_economy else None
 
     draw = ImageDraw.Draw(image)
-    draw.text((50, 20), ball.short_name or ball.country, font=title_font)
+    draw.text((50, 90), ball.short_name or ball.country, font=title_font)
     for i, line in enumerate(textwrap.wrap(f"Ability: {ball.capacity_name}", width=26)):
         draw.text(
-            (100, 1050 + 100 * i),
+            (70, 1070 + 75 * i),
             line,
             font=capacity_name_font,
             fill=(255, 255, 255, 255),
             stroke_width=2,
             stroke_fill=(0, 0, 0, 255),
         )
-    for i, line in enumerate(textwrap.wrap(ball.capacity_description, width=32)):
+    for i, line in enumerate(textwrap.wrap(ball.capacity_description, width=40)):
         draw.text(
-            (60, 1300 + 80 * i),
+            (60, 1370 + 50 * i),
             line,
             font=capacity_description_font,
             stroke_width=1,
             stroke_fill=(0, 0, 0, 255),
         )
     draw.text(
-        (320, 1670),
+        (320, 1710),
         str(ball_instance.health),
         font=stats_font,
         fill=ball_health,
@@ -67,7 +67,7 @@ def draw_card(ball_instance: "BallInstance"):
         stroke_fill=(0, 0, 0, 255),
     )
     draw.text(
-        (1120, 1670),
+        (1120, 1710),
         str(ball_instance.attack),
         font=stats_font,
         fill=(255, 255, 255, 255),
